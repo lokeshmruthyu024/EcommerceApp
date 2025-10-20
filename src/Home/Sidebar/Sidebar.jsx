5a06ad21cc819cbcec863d9d17685359f762b9cf
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2025-10-20 20:56:04 +0530

    homepage

---
+++ src/Home/Sidebar/Sidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='py-4'>
+      <ul className='list-unstyled sideBarul'>
+        <li><a href="">Women's Fashion</a></li>
+        <li><a href="">Men's Fashion</a></li>
+        <li><a href="">Electronics</a></li>
+        <li><a href="">Home & Lifestyle</a></li>
+        <li><a href="">Medicine</a></li>
+        <li><a href="">Sports & Outdoor</a></li>
+        <li><a href="">Baby's & Toys</a></li>
+        <li><a href="">Groceries's & Pets</a></li>
+        <li><a href="">Health & Beauty</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
